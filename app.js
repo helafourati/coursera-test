4e430387533fcf254dd550c2a44ab01be3f023a2
fourati hela <halloula.fourati1@gmail.com>
2023-06-26 15:01:28 +0200

    next version of file

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
-(function (){
-  angular.module('myFirstApp')
-  .controller('MyFirstController', function){
+(function () {
+  'use strict';
+  angular.module('myFirstApp', [] )
+  .controller('MyFirstController', function ($scope) {
+    $scope.name ="Hela";
+    $scope.sayHello = function () {
+      return "Hello Coursera!";
+
+    };
 
   });
 })();
